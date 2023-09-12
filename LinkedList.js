c0eb08cf04ecce5ed69f62df6519d68c0b196740
Wojciech Chuchro <wojciech.chuchro@pm.me>
2023-09-12 20:24:13 +0200

    Add prettier and eslint to the project

--- LinkedList.js
+++ LinkedList.js
@@ -1,46 +1,46 @@
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        };
-        this.tail = this.head;
-        this.length = 1;
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let str = "HEAD -> ";
+    while (currentNode !== null) {
+      str += `${currentNode.value} -> `;
+      currentNode = currentNode.next;
     }
-
-    append(value) {
-        const newNode = new Node(value)
-
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
-    }
-
-    prepend(value) {
-        const newNode = new Node(value)
-
-        newNode.next = this.head
-        this.head = newNode
-        this.length++;
-    }
-
-    printList() {
-        let currentNode = this.head;
-        let str = "HEAD -> ";
-        while(currentNode !== null) {
-            str += `${currentNode.value} -> `;
-            currentNode = currentNode.next;
-        }
-        str += "NULL";
-        console.log(str);
-    }
+    str += "NULL";
+    console.log(str);
+  }
 }
 
 let myLinkedList = new LinkedList(10);
@@ -50,5 +50,3 @@ myLinkedList.append(16);
 myLinkedList.prepend(1);
 
 myLinkedList.printList();
-
-
