924cf65c9ca0396e768577e157d060b39b2ac4ed
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-11-19 19:42:59 +0100

    Upgrade two sum algorithm

--- Java/LeetCode/TwoSum.java
+++ Java/LeetCode/TwoSum.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.LeetCode;
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -26,10 +26,14 @@ public class TwoSum {
     }
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    return new int[] {i, j};
-                }
+            for (int j = 0; j < nums.length; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] {i, j};
+                    }
             }
         }
         return new int[] {};
