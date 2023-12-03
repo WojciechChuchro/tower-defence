789e0a2b736080bbeac9fe507907d68c34c241e4
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-03 16:44:05 +0100

    Create factorial algorithm using recursion

---
+++ Java/LeetCode/FactorialRecursion.java
@@ -0,0 +1,16 @@
+package Java.LeetCode;
+
+public class FactorialRecursion {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int number) {
+        if (number == 2) {
+            return 2;
+        }
+
+        return number * factorial(number - 1);
+    }
+}
