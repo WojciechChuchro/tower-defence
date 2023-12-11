432f5d03d0e52ae2f727197f6c562bceb9606f4c
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-11 19:35:13 +0100

    Create bubble sort algorithm

--- Java/sort/BubbleSort.java
+++ Java/sort/BubbleSort.java
@@ -1,15 +1,24 @@
 package Java.sort;
 
+import java.util.Collections;
 import java.util.List;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        System.out.println(sort(List.of(3, 5, 3, 2  )));
+        List<Integer> sortedList = sort(List.of(3, 5, 3, 2 ));
+        System.out.println(sortedList);
     }
 
     public static List<Integer> sort(List<Integer> array) {
+        int length = array.size();
 
-
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array.get(j) > array.get(j + 1)) {
+                    Collections.swap(array, j, j + 1);
+                }
+            }
+        }
 
         return array;
     }
