cc645adfae9fbb32fe662028de61dcbf8aa6d8af
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-11 19:18:27 +0100

    Initial commit

---
+++ Java/sort/BubbleSort.java
@@ -0,0 +1,16 @@
+package Java.sort;
+
+import java.util.List;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        System.out.println(sort(List.of(3, 5, 3, 2  )));
+    }
+
+    public static List<Integer> sort(List<Integer> array) {
+
+
+
+        return array;
+    }
+}
