d67c52cd107b7753ef574865f7888da2a76a5b00
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-15 07:19:24 +0100

    Implement bubble sort algorithm

--- Java/sort/BubbleSort.java
+++ Java/sort/BubbleSort.java
@@ -1,20 +1,33 @@
 package Java.sort;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
-public class BubbleSort {
-    public static List<Integer> sort(List<Integer> array) {
-        int length = array.size();
+public class BubbleSort implements ISort {
+
+    public static void main(String[] args) {
+        SortConstant sortConstant = new SortConstant();
+        BubbleSort bubbleSort = new BubbleSort();
+
+        bubbleSort.sort(sortConstant.stringArray);
+        System.out.println("Sorted String Array: " + Arrays.toString(sortConstant.stringArray));
+
+        bubbleSort.sort(sortConstant.intArray);
+        System.out.println("Sorted Integer Array: " + Arrays.toString(sortConstant.intArray));
+    }
+
+    @Override
+    public <T extends Comparable<T>> void sort(T[] array) {
+        int length = array.length;
 
         for (int i = 0; i < length - 1; i++) {
             for (int j = 0; j < length - i - 1; j++) {
-                if (array.get(j) > array.get(j + 1)) {
-                    Collections.swap(array, j, j + 1);
+                if ((array[j]).compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
 
-        return array;
     }
 }
