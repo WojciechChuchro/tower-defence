311eebf783882fb7b14ddce49bb3f57d7b492bb3
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-15 20:44:23 +0100

    Create constant class

---
+++ Java/sort/SortConstant.java
@@ -0,0 +1,10 @@
+package Java.sort;
+
+public class SortConstant {
+
+    public final String[] stringArray = {"banana", "apple", "orange", "grape", "kiwi"};
+    public final Integer[] intArray = {5, 2, 8, 1, 7};
+
+//    Sorted String Array: [apple, banana, grape, kiwi, orange]
+//    Sorted Integer Array: [1, 2, 5, 7, 8]
+}
