afa7625b2a26ad1c3b9fde05c4846cb9312b917e
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-12-16 13:35:39 +0100

    Refactor constant names

--- Java/sort/SortConstant.java
+++ Java/sort/SortConstant.java
@@ -2,8 +2,8 @@ package Java.sort;
 
 public class SortConstant {
 
-    public final String[] stringArray = {"banana", "apple", "orange", "grape", "kiwi"};
-    public final Integer[] intArray = {5, 2, 8, 1, 7};
+    public final String[] STRING_ARRAY = {"banana", "apple", "orange", "grape", "kiwi"};
+    public final Integer[] INT_ARRAY = {5, 2, 8, 1, 7};
 
 //    Sorted String Array: [apple, banana, grape, kiwi, orange]
 //    Sorted Integer Array: [1, 2, 5, 7, 8]
