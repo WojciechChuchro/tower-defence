0f4da1d399b9e1f9a8b4dba19806bde231eb636b
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-12-13 08:09:48 +0100

    Implement TakeGiftsFromTheRichestPile algorithm

---
+++ Java/LeetCode/TakeGiftsFromTheRichestPile.java
@@ -0,0 +1,33 @@
+class TakeGiftsFromTheRichestPile {
+
+    public static void main(String[] args) {
+        int[] arr = {
+            25,64,9,4,100
+        };
+        TakeGiftsFromTheRichestPile tgftrp = new TakeGiftsFromTheRichestPile();
+
+        System.out.println(tgftrp.pickGifts(arr, 4));
+    }
+
+    public long pickGifts(int[] gifts, int k) {
+        int maxIdx = 0;
+        while (k > 0) {
+            for (int i = 0; i < gifts.length; i++) {
+                if (gifts[maxIdx] < gifts[i]) {
+                    maxIdx = i;
+                }
+            }
+
+            gifts[maxIdx] = (int) Math.floor(Math.sqrt(gifts[maxIdx]));
+
+            k--;
+        }
+        
+        long sum = 0l;
+        for(int g : gifts) {
+            sum += g;
+        }
+        
+        return sum;
+    }
+}
